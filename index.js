cfe2c704212e50a4dd64a003bb78551fb369e757
Kenneth Kebaara <kkebaara@yahoo.com>
2021-08-18 18:15:46 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+
+function appendCat(name){
+ const app2 = [...cats, name];
+ return app2
+}
+
+function prependCat(name){
+    const app2 = [name, ...cats];
+    return app2;
+}
+
+function removeLastCat(){
+  return cats.slice(0, cats.length - 1);
+
+}
+
+function removeFirstCat(){
+ return cats.slice(1)
+
+}
